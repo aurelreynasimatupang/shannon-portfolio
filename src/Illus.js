807694697fa670a05b0e8871b41636ada5b82faf
Aurelia F. Simatupang <dorm4ever@gmail.com>
2023-12-05 14:28:14 +0100

    fix build bug

--- src/Illus.js
+++ src/Illus.js
@@ -16,18 +16,18 @@ function Illus(props) {
                 <div class="illus-gallery">
                     <div class="illus-gallery-2">
                         <div class="card card-landscape">
-                            <img src={require("./img/img3.jpg")} />
+                            <img src={require("./img/img3.jpg")} alt="Illustration by Louisa Shannon Tara" title="Illustration by Louisa Shannon Tara"/>
                             <h3 class="fade-in-text">Color Study #1</h3>
                             <Link to="/commission" class="btn btn-purple fade-in-text">Commission This</Link>
                         </div>
                         <div class="card card-landscape">
-                            <img src={require("./img/img2.jpg")} />
+                            <img src={require("./img/img2.jpg")} alt="Illustration by Louisa Shannon Tara" title="Illustration by Louisa Shannon Tara"/>
                             <h3 class="fade-in-text">Color Studey #2</h3>
                             <Link to="/commission" class="btn btn-purple fade-in-text">Commission This</Link>
                         </div>
                     </div>
                     <div class="card card-portrait">
-                        <img src={require("./img/img4.jpg")} />
+                        <img src={require("./img/img4.jpg")} alt="Illustration by Louisa Shannon Tara" title="Illustration by Louisa Shannon Tara" />
                         <h3 class="fade-in-text">Illustration</h3>
                         <Link to="/commission" class="btn btn-purple fade-in-text">Commission This</Link>
                     </div>
